0b22d71c946db3781765aa8645e9459758b02297
Demidss <121161368+Demidss@users.noreply.github.com>
2023-06-21 12:52:42 +0200

    .

--- Assets/ScoreSystem.cs
+++ Assets/ScoreSystem.cs
@@ -7,16 +7,16 @@ public class ScoreSystem : MonoBehaviour
 {
     public Text scoreText;
     public int score;
-    public string sceneToLoad;
+    public string sceneToLoad = "Scene"; // Assign the scene name here
 
     void Update()
     {
         score = (int)Time.time;
         scoreText.text = score.ToString();
 
-        if (score >= 69)
+        if (score >= 5)
         {
-            SceneManager.LoadScene('Level2');
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 }
